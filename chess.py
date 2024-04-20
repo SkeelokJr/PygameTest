7242ffe3e21145d319c9b38be91490521e7ee3f5
SkeelokJr <167640044+SkeelokJr@users.noreply.github.com>
2024-04-20 14:31:12 -0500

    Update chess.py
    added board and squares

--- chess.py
+++ chess.py
@@ -1,18 +1,65 @@
 import pygame
-print(pygame.ver)
+import math
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+sWidth, sHeight = 1280, 720
+screen = pygame.display.set_mode((sWidth, sHeight))
 clock = pygame.time.Clock()
 running = True
+
+# create board
+boardBorderColor, boardColor = pygame.Color(255, 255, 255), pygame.Color(145, 100, 80)
+boardSize = 500
+boardBorderWidth = 3
+boardX, boardY = sWidth/2 - boardSize/2, sHeight/2 - boardSize/2
+boardBorder = pygame.Rect(boardX, boardY, boardSize, boardSize)
+boardMiddle = pygame.Rect(boardX+boardBorderWidth, boardY+boardBorderWidth, boardSize-2*boardBorderWidth, boardSize-2*boardBorderWidth)
+
+#create squares [file(a=0,h=7)][rank(0-7)]
+numSquares = 64
+lightColor = pygame.Color(255,255,255)
+darkColor = pygame.Color(0,0,0)
+boardGap = 50
+Squares = []
+for i in range(numSquares):
+    file, rank = int(i / 8), i % 8
+    squareColor = None
+    if (file + rank) % 2 == 0:
+        squareColor = darkColor
+    else:
+        squareColor = lightColor
+    squareSize = round((boardSize - 2*boardGap) / 8)
+    squareX = boardX + boardGap + (file*squareSize)
+    squareY = (boardY+boardSize) - 2*boardGap - ((rank)*squareSize)
+    squareRect = pygame.Rect(squareX, squareY, squareSize, squareSize)
+    squareData = {
+        "file": file,
+        "rank": rank,
+        "color": squareColor,
+        "rect": squareRect
+    }
+    Squares.append(squareData)
+
+def drawBoard():
+    pygame.draw.rect(screen, boardBorderColor, boardBorder)
+    pygame.draw.rect(screen, boardColor, boardMiddle)
+
+def drawSquares():
+    for s in Squares:
+        pygame.draw.rect(screen, s["color"], s["rect"])
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill("black")
+
+    drawBoard()
+    drawSquares()
+    
+
 
     pygame.display.flip()
 
