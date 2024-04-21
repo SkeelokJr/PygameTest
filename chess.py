62904a668d0e3841fea77822a4a632e34ee35ca7
SkeelokJr <167640044+SkeelokJr@users.noreply.github.com>
2024-04-20 22:06:46 -0500

    Update chess.py
    pawns work

--- chess.py
+++ chess.py
@@ -20,6 +20,7 @@ boardMiddle = pygame.Rect(boardX+boardBorderWidth, boardY+boardBorderWidth, boar
 numSquares = 64
 lightColor = pygame.Color(255,255,255)
 darkColor = pygame.Color(31,31,127)
+hoverColor = pygame.Color(200,200,200)
 boardGap = 50
 Squares = []
 for i in range(numSquares):
@@ -55,7 +56,63 @@ def drawBoard():
 
 def drawSquares():
     for s in Squares:
-        pygame.draw.rect(screen, s["color"], s["rect"])
+        if s["highlight_color"]:
+            pygame.draw.rect(screen, s["color"].lerp(s["highlight_color"][-1], 0.5), s["rect"])
+        else:
+            pygame.draw.rect(screen, s["color"], s["rect"])
+
+def getPieceAt(file, rank):
+    for piece in Pieces:
+        if piece["file"] == file and piece["rank"] == rank:
+            return piece
+    return None
+
+def getMovesOfPiece(piece):
+    moves = []
+    file, rank = piece["file"], piece["rank"]
+    color = piece["color"]
+    match piece["type"]:
+        case "pawn":
+            if color == "white":
+                if not getPieceAt(file, rank+1) and rank < 7: 
+                    moves.append((file, rank+1))
+                    if not piece["hasMoved"] and rank < 6 and not getPieceAt(file, rank+2):
+                        moves.append((file, rank+2))
+                if file > 0 and rank < 7:
+                    other = getPieceAt(file-1, rank+1)
+                    if other and other["color"] == "black":
+                        moves.append((file-1,rank+1))
+                if file < 7 and rank < 7:
+                    other = getPieceAt(file+1, rank+1)
+                    if other and other["color"] == "black":
+                        moves.append((file+1,rank+1))
+            else:
+                if not getPieceAt(file, rank-1) and rank > 0: 
+                    moves.append((file, rank-1))
+                    if not piece["hasMoved"] and rank > 1 and not getPieceAt(file, rank-2):
+                        moves.append((file, rank-2))
+                if file > 0 and rank > 0:
+                    other = getPieceAt(file-1, rank-1)
+                    if other and other["color"] == "white":
+                        moves.append((file-1,rank-1))
+                if file < 7 and rank > 0:
+                    other = getPieceAt(file+1, rank-1)
+                    if other and other["color"] == "white":
+                        moves.append((file+1,rank-1))
+        case "knight":
+            pass
+        case "bishop":
+            pass
+        case "rook":
+            pass
+        case "queen":
+            pass
+        case "king":
+            pass
+    return moves
+
+        
+
 
 def createPiece(color, type, file, rank):
     fil = ""
@@ -87,6 +144,7 @@ def createPiece(color, type, file, rank):
         "type": type,
         "file": file,
         "rank": rank,
+        "hasMoved": False,
         "image": pieceImage,
         "rect": pieceRect
     }
@@ -96,19 +154,40 @@ def setBoard():
     for i in range(8):
         createPiece("white", "pawn", i, 1)
         createPiece("black", "pawn", i, 6)
+    createPiece("white", "pawn", 2, 2)
+    createPiece("white", "pawn", 3, 3)
+    createPiece("black", "pawn", 5, 2)
+    createPiece("black", "pawn", 6, 3)
 
 def drawPieces():
     for piece in Pieces:
         screen.blit(piece["image"], piece["rect"])
 
 def coordsToFileRank(x, y):
-    file = 
+    if x >= boardX+boardGap and x < boardX+boardSize-boardGap and y > boardY+boardGap and y <= boardY+boardSize-boardGap:
+        file = int((x - boardX - boardGap) / squareSize)
+        rank = int((boardY + boardSize - 2*boardGap - y) / squareSize + 1)
+        return (file, rank)
+    return (None, None)
 
 def getMouseInfo():
+    global mouseFile, mouseRank
     mousePos = pygame.mouse.get_pos()
     mouseX, mouseY = mousePos[0], mousePos[1]
-    mouseFile, mouseRank = 
-    print(mouseX, mouseY)
+    mouseLoc = coordsToFileRank(mouseX, mouseY)
+    mouseFile, mouseRank = mouseLoc[0], mouseLoc[1]
+    for piece in Pieces:
+        if piece["file"] == mouseFile and piece["rank"] == mouseRank:
+            print(getMovesOfPiece(piece))
+
+def highlightSquare(file, rank, color):
+    for square in Squares:
+        if square["file"] == file and square["rank"] == rank:
+            square["highlight_color"].append(color)
+
+def clearHighlights():
+    for square in Squares:
+        square["highlight_color"].clear()
 
 setBoard()
 
@@ -117,11 +196,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill("black")
+    clearHighlights()
 
     drawBoard()
+    getMouseInfo()
+    highlightSquare(mouseFile, mouseRank, hoverColor)
+    
     drawSquares()
     drawPieces()
-    getMouseInfo()
+    
 
     pygame.display.flip()
 
