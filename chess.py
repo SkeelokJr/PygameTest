803637c03b066b7d2ca1dfe645dfd80a6d9d4b6e
SkeelokJr <167640044+SkeelokJr@users.noreply.github.com>
2024-04-21 09:05:43 -0500

    Update chess.py
    finished pieces, added mouse clicks

--- chess.py
+++ chess.py
@@ -1,5 +1,4 @@
-import pygame
-import math
+import pygame, math
 
 #setup
 pygame.init()
@@ -49,7 +48,12 @@ Pieces = []
 
 # initialize dynamic variables
 mouseX, mouseY = 0, 0
+mouseClicks = []
 mouseFile, mouseRank = None, None
+selectedPiece = None
+selectedFile, selectedRank = None, None
+playerTurn = "white"
+boardFlipped = False
 
 def drawBoard():
     pygame.draw.rect(screen, boardBorderColor, boardBorder)
@@ -136,12 +140,110 @@ def getMoves(piece):
                         break
                 else:
                     moves.append((file+d, rank+d))
+            for d in range(1, min(8-file, rank+1)): # bottom right diagonal
+                other = getPieceAt(file+d, rank-d)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file+d, rank-d))
+                        break
+                else:
+                    moves.append((file+d, rank-d))
+            for d in range(1, min(file+1, 8-rank)): # top left diagonal
+                other = getPieceAt(file-d, rank+d)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file-d, rank+d))
+                        break
+                else:
+                    moves.append((file-d, rank+d))
+            for d in range(1, min(file+1, rank+1)): # bottom left diagonal
+                other = getPieceAt(file-d, rank-d)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file-d, rank-d))
+                        break
+                else:
+                    moves.append((file-d, rank-d))
         case "rook":
-            pass
-        case "queen":
-            pass
+            for d in range(1, 8-rank): # get up moves
+                other = getPieceAt(file,rank+d)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file, rank+d))
+                        break
+                else:
+                    moves.append((file, rank+d))
+            for d in range(1, 8-file): # get moves right
+                other = getPieceAt(file+d,rank)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file+d, rank))
+                        break
+                else:
+                    moves.append((file+d, rank))
+            for d in range(1, rank+1): # get down moves
+                other = getPieceAt(file,rank-d)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file, rank-d))
+                        break
+                else:
+                    moves.append((file, rank-d))
+            for d in range(1, file+1): # get left moves
+                other = getPieceAt(file-d,rank)
+                if other:
+                    if other["color"] == color:
+                        break
+                    else:
+                        moves.append((file-d, rank))
+                        break
+                else:
+                    moves.append((file-d, rank))
+        case "queen": # basically combine a bishop and rook's moves
+            bishop = {"type": "bishop", "file": file, "rank": rank, "color": color}
+            bishopMoves = getMoves(bishop)
+            rook = {"type": "rook", "file": file, "rank": rank, "color": color}
+            rookMoves = getMoves(rook)
+            moves.extend(bishopMoves)
+            moves.extend(rookMoves)
         case "king":
-            pass
+            other = getPieceAt(file, rank+1) # up
+            if rank <= 6 and (not other or not other["color"] == color):
+                moves.append((file,rank+1))
+            other = getPieceAt(file+1, rank+1) # up right
+            if file <= 6 and rank <= 6 and (not other or not other["color"] == color):
+                moves.append((file+1,rank+1))
+            other = getPieceAt(file+1, rank) # right
+            if file <= 6 and (not other or not other["color"] == color):
+                moves.append((file+1,rank))
+            other = getPieceAt(file+1, rank-1) # down right
+            if file <= 6 and rank >= 1 and (not other or not other["color"] == color):
+                moves.append((file+1,rank-1))
+            other = getPieceAt(file, rank-1) # down
+            if rank >= 1 and (not other or not other["color"] == color):
+                moves.append((file,rank-1))
+            other = getPieceAt(file-1, rank-1) # down left
+            if file >= 1 and rank >= 1 and (not other or not other["color"] == color):
+                moves.append((file-1,rank-1))
+            other = getPieceAt(file-1, rank) # left
+            if file >= 1 and (not other or not other["color"] == color):
+                moves.append((file-1,rank))
+            other = getPieceAt(file-1, rank+1) # up left
+            if file >= 1 and rank <= 6 and (not other or not other["color"] == color):
+                moves.append((file-1,rank+1))
+                
     return moves
 
 def createPiece(color, type, file, rank):
@@ -180,8 +282,6 @@ def createPiece(color, type, file, rank):
     }
     Pieces.append(pieceData)
 
-
-
 def drawPieces():
     for piece in Pieces:
         screen.blit(piece["image"], piece["rect"])
@@ -194,15 +294,15 @@ def coordsToFileRank(x, y):
     return (None, None)
 
 def getMouseInfo():
-    global mouseFile, mouseRank
+    global mouseFile, mouseRank, mouseClicks
     mousePos = pygame.mouse.get_pos()
+    mouseClicks = pygame.mouse.get_pressed()
     mouseX, mouseY = mousePos[0], mousePos[1]
     mouseLoc = coordsToFileRank(mouseX, mouseY)
     mouseFile, mouseRank = mouseLoc[0], mouseLoc[1]
     for piece in Pieces:
         if piece["file"] == mouseFile and piece["rank"] == mouseRank:
             moves = getMoves(piece)
-            print(*moves)
             for move in moves:
                 highlightSquare(*move, moveColor)
 
@@ -217,27 +317,49 @@ def clearHighlights():
 
 def setBoard():
     for i in range(8):
-        createPiece("white", "bishop", i, 1)
-        createPiece("black", "bishop", i, 6)
-    createPiece("white", "pawn", 2, 2)
-    createPiece("white", "pawn", 3, 3)
-    createPiece("black", "pawn", 5, 2)
-    createPiece("black", "pawn", 6, 3)
-    createPiece("white", "pawn", 4, 7)
+        createPiece("white", "pawn", i, 1)
+        createPiece("black", "pawn", i, 6)
+    createPiece("white", "rook", 0, 0)
+    createPiece("white", "rook", 7, 0)
+    createPiece("white", "knight", 1, 0)
+    createPiece("white", "knight", 6, 0)
+    createPiece("white", "bishop", 2, 0)
+    createPiece("white", "bishop", 5, 0)
+    createPiece("white", "queen", 3, 0)
+    createPiece("white", "king", 4, 0)
+    createPiece("black", "rook", 0, 7)
+    createPiece("black", "rook", 7, 7)
+    createPiece("black", "knight", 1, 7)
+    createPiece("black", "knight", 6, 7)
+    createPiece("black", "bishop", 2, 7)
+    createPiece("black", "bishop", 5, 7)
+    createPiece("black", "queen", 3, 7)
+    createPiece("black", "king", 4, 7)
+
+def LMB_Down():
+    global selectedFile, selectedRank
+    if not selectedPiece:
+        selectedPiece = getPieceAt(mouseFile,mouseRank)
+    print("LMB pressed")
+
 
 setBoard()
 
 while running:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    mouseClicks = pygame.mouse.get_pressed()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if mouseClicks[0]:
+                LMB_Down()
     screen.fill("black")
     clearHighlights()
 
     drawBoard()
     getMouseInfo()
     highlightSquare(mouseFile, mouseRank, hoverColor)
-    
     drawSquares()
     drawPieces()
     
